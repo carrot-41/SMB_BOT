b2febb7b432860e771fcd39bac01e558cdb261ac
carrot_41 <carrot41minecraft@gmail.com>
2026-02-26 03:43:28 +0900

    안쓰는 임포트문 삭제

--- src/main/java/my/bot/BotMain.java
+++ src/main/java/my/bot/BotMain.java
@@ -1,7 +1,6 @@
 package my.bot;
 
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import response.ListenCommend;
+import BanWord.MessageFilter;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
